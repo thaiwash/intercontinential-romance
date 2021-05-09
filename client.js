e89e6699f0875845861d6d3fb500d4c2aea40b9a
Taivas Gogoljuk <taivas.gogoljuk@gmail.com>
2021-05-09 23:15:19 +0300

    autogit

--- client.js
+++ client.js
@@ -1,3 +1,19 @@
 // get [url]
 // execute every 10 sec
 // Send output to server
+
+var http = require("http")
+
+var options = {
+  host: 'avros.tk',
+  port: 3000,
+  path: '/get_command'
+};
+
+var interval = setInterval(function() {
+  http.get(options, function(res) {
+    console.log("Got response: " + res.statusCode);
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+}, 1000)
--- server.js
+++ server.js
@@ -6,12 +6,54 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require("fs")
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('public/terminal.html')
+  res.send(fs.readFileSync('public/terminal.html').toString("utf-8"))
 })
+
+var command = ""
+var reply = ""
+
+
+//send command
+app.get('/send_command', (req, res) => {
+  command = req.query.cmd
+  console.log("Command '"+command+ "' was received")
+  res.send('OK')
+})
+
+// reply if message in queue
+app.get('/send_reply', (req, res) => {
+  reply = req.query.reply
+  console.log("Reply '"+reply+ "' was given")
+  res.send('OK')
+
+})
+
+// reply if message in queue
+app.get('/get_reply', (req, res) => {
+  if(reply.length > 0) {
+    var ret = reply+""
+    reply = ""
+    res.send(ret)
+  }
+  res.send('OK')
+})
+
+//take command from queue
+app.get('/get_command', (req, res) => {
+  if(command.length > 0) {
+    console.log("Command '"+command+ "' was given")
+    var ret = command+""
+    command = ""
+    res.send(ret)
+  }
+  res.send('OK')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
